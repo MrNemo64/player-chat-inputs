6897ab2aa514ab34deab427e79262f4c9407f449
Hasan Demirtaş <utsukushihito@outlook.com>
2020-05-11 14:54:12 +0300

    Update PlayerChatInput.java

--- src/main/java/io/github/nemo_64/chatinput/PlayerChatInput.java
+++ src/main/java/io/github/nemo_64/chatinput/PlayerChatInput.java
@@ -202,7 +202,7 @@ public final class PlayerChatInput<T> implements Listener {
     public void start() {
         this.plugin.getServer().getPluginManager().registerEvents(this, this.plugin);
         if (this.expire != -1L) {
-            this.expireTask = Bukkit.getScheduler().runTaskLaterAsynchronously(this.plugin, () ->
+            this.expireTask = Bukkit.getScheduler().runTaskLater(this.plugin, () ->
                 this.getExpireTask()
                     .filter(task -> !task.isCancelled())
                     .ifPresent(task -> this.onExpire.accept(this.player)), this.expire);
